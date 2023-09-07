247bcf2db90cba776376ce00efa04192a1256c19
panzongqing <176578202@qq.com>
2023-09-07 18:03:57 +0800

    update

---
+++ models/models.go
@@ -0,0 +1,11 @@
+package models
+
+import (
+	_ "github.com/jinzhu/grom"
+)
+
+var DB *grom.DB
+
+func main() {
+
+}
--- pkg/util/pagination.go
+++ pkg/util/pagination.go
@@ -9,7 +9,7 @@ import (
 
 func GetPage(c *gin.Context) int {
 	result := 0
-	page, _ := com.StrTo(c.Query("page")).int()
+	page, _ := com.StrTo(c.Query("page")).Int()
 	if page > 0 {
 		result = (page - 1) * setting.PageSize
 	}
